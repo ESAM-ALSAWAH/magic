56338cfb810fc6dfd7729245eb6b057fbfd4410e
ESAM-ALSAWAH <esam.alsawah2000@gmail.com>
2022-05-29 09:37:52 +0300

    update

--- pages/group/specialone.tsx
+++ pages/group/specialone.tsx
@@ -74,7 +74,7 @@ const Specialone: NextPage = () => {
         <img
           src="/images/shape.svg"
           alt="shape"
-          className="absolute -left-[200px] -bottom-[100px] "
+          className="absolute -left-[200px] -bottom-[120px] "
           style={{ zIndex: -1 }}
           loading="lazy"
         />
